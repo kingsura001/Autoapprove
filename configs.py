6c9e08b66b9506aabb23888d3ef86b2535e6ccf9
kingsura001 <kingsura001@gmail.com>
2025-07-14 21:22:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,14 +6,14 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "27940539"))
+    API_HASH = getenv("API_HASH", "2390dfdb25011dd1dac1de83cd68946b")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8196124006:AAFT6cYvREZxyv81S4SRFOGA6zXzwM4Xhpw")
     # Your Force Subscribe Channel Id Below 
-    CHID = int(getenv("CHID", "")) # Make Bot Admin In This Channel
+    CHID = int(getenv("CHID", "-1002549323332")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
-    SUDO = list(map(int, getenv("SUDO", "").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    SUDO = list(map(int, getenv("SUDO", "6954573092").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Kingsura001:kingsura875744@clusterbot.rj0n2ys.mongodb.net/?retryWrites=true&w=majority&appName=Clusterbot")
     
 cfg = Config()
 
